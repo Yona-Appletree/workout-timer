764d128222ec034b77c31f5b01abfb9c8ddc0af1
Yona Appletree <hypher@gmail.com>
2025-04-27 12:25:54 -0700

    remix router

---
+++ src/router.ts
@@ -0,0 +1,5 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { routes } from 'virtual:routes';
+
+// from https://vite-remix-router.vercel.app/guides/getting-started.html
+export const router = createBrowserRouter(routes);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import RemixRouter from 'vite-plugin-remix-router';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), RemixRouter()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
